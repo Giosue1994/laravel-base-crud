c38ed2a2cbf5974b23108e235eb3b5058e6fce06
Giosue1994 <giosuelentini1994@outlook.com>
2020-09-03 19:19:02 +0200

    eliminare movie

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -12,8 +12,15 @@
     <div>
       @foreach ($movies as $movie)
         <h2><a href="{{ route('movies.show', $movie->id) }}">{{ $movie->title }}</a></h2>
+        <p><b>Descrizione:</b> {{ $movie->description }}</p>
         <button type="button" name="button"><a href="{{ route('movies.edit', $movie->id) }}">Modifica</a></button>
-        <button type="button" name="button"><a href="{{ route('movies.destroy', $movie->id) }}">Elimina</a></button>
+
+        <form action="{{ route('movies.destroy', $movie->id) }}" method="post">
+          @csrf
+          @method('DELETE')
+
+          <input type="submit" value="Elimina">
+        </form>
         <hr>
       @endforeach
     </div>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,9 +3,9 @@
 @section('main_content')
   <div>
     <h1>{{ $movie->title }}</h1>
-    <p>Anno: {{ $movie->year }}</p>
-    <p>Rating: {{ $movie->rating }}</p>
-    <p>Descrizione: {{ $movie->description }}</p>
+    <p><b>Anno:</b> {{ $movie->year }}</p>
+    <p><b>Rating:</b> {{ $movie->rating }}</p>
+    <p><b>Descrizione:</b> {{ $movie->description }}</p>
 
     <p>
       <a href="{{ route('movies.index') }}">Torna alla home</a>
